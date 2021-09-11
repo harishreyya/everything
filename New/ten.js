4fb69af1b196ccef85986cf802fa10f1228a9b25
Harisintu <harish_fw11_240@masai.school>
2021-09-11 22:43:46 +0530

    input assignment 18

---
+++ New/ten.js
@@ -0,0 +1,22 @@
+function runProgram(input){
+    var a = Number(input);
+    console.log((a*3)+ 10);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+   read += input;
+});
+process.stdin.on("end", function () {
+   read = read.replace(/\n$/,"")
+  runProgram(read);
+});
+
+process.on("SIGINT", function () {
+   read = read.replace(/\n$/,"")
+   runProgram(read);
+   process.exit(0);
+});
+
