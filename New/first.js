ce9f251ca6920122e20421c83e980c07c599626b
Harisintu <harish_fw11_240@masai.school>
2021-09-18 15:17:02 +0530

    assignment

---
+++ New/first.js
@@ -0,0 +1,35 @@
+
+function runProgram(input) {
+ input =  input.trim().split("\n")
+ var [row,col]= input[0].trim().split(" ").map(Number);
+ var line=1;
+ for(i=0;i<row;i++){
+     var arr = input[line++].trim().split(" ").map(Number);
+     console.log(arr.join(" "))
+ }
+ 
+}
+
+if (process.env.USERNAME === 'haris') {
+  runProgram(`3 2
+  1 2
+  3 4
+  5 6`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
