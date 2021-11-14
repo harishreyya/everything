c7e7adca798377135c2d28b6b92037c6191babce
harishreyya <harishrebel0@gmail.com>
2021-11-14 21:13:24 +0530

    food app

---
+++ mini app assignments/foodapp/components/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return ` 
+
+
+    <div id="options">
+    <img src="https://1000logos.net/wp-content/uploads/2021/01/Dominos-logo.jpg"/>
+    <h3><a href="index.html">Home</a></h3>
+    <h3><a href="recipe.html">Recipe of the day</a></h3>
+    <h3><a href="latest.html">Latest recipes</a></h3>
+   
+    <input id="name" type="text" placeholder="search for meals">
+ 
+ </div>`
+}
+
+export default navbar
